ba474112c21561882741d2d761b5ed1b5c6e127c
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-24 09:40:04 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Francesca MACDONALD on 2023-08-24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
